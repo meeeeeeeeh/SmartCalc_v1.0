b05bb5f6922ba8fdf4a51919837f8d9d4580d0fe
Polina <polina_zolotova_qrp@mail.ru>
2023-08-24 23:31:35 +0300

    push and tests for it

--- src/stack.c
+++ src/stack.c
@@ -10,6 +10,10 @@ struct Node {
     struct Node * next; // указатель на след элемент
 };
 
+void print(Node * list);
+void push(Node ** plist, Data d);
+
+
 void print(Node * list) {
     for (Node * p = list; p != NULL; p = p->next) {
         printf("%d ", p->data);
@@ -17,16 +21,22 @@ void print(Node * list) {
     printf("\n");
 }
 
+void push(Node ** plist, Data d) { // 1 арг - указатель на указатель последнего элемента
+    Node * p = malloc(sizeof(Node));
+    p->data = d;
+    p->next = *plist; // разыменование plist
+    *plist = p;
+}
+
 
 int main() {
+    Data test[] = {21, 17, 3, 10};
     Node * list = NULL;
-    Node a = {3}, b = {17}, c = {21}, t = {10};
-    list = &a;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
 
-    print(list);
+    for (size_t i = 0; i < sizeof(test)/ sizeof(test[0]); i++) {
+        push(&list, test[i]); // передаем указатель потому что переменная будет меняться
+        print(list);
+    }
 
     return 0;
 }
